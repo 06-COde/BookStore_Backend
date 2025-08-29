ba3159be167274a6731839bdcb22e5ef47dca09e
Shashi <rishav.sinha477@gmail.com>
2025-08-29 15:31:05 +0530

    final code with sanitization

--- server.js
+++ server.js
@@ -4,23 +4,43 @@ import 'dotenv/config';
 import connectDB from './config/connectDB.js';  
 import bookRoutes from './routes/book.routes.js';
 import errorHandler from './middlewares/error.handler.js';
+import http from 'http';
+import os from 'os';
+import cluster from 'cluster';
 
-const app = express();
+const totalCpus = os.cpus().length;
 const PORT = process.env.PORT || 5000;
+const app = express();
 
 app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
-
-connectDB();
-
 app.use('/api/books', bookRoutes);   
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
   res.status(200).json({ success: true, message: "Hello World!" });
 });
 
-app.use(errorHandler);
+const startServer = async () => {
+  await connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  if (cluster.isPrimary) {
+    console.log(`🔹 Primary with pid ${process.pid} is running`);
+
+    for (let i = 0; i < totalCpus; i++) {
+      cluster.fork();
+    }
+
+    cluster.on('exit', (worker) => {
+      console.error(`Worker ${worker.process.pid} died. Restarting...`);
+      cluster.fork();
+    });
+  } else {
+    const server = http.createServer(app);
+    server.listen(PORT, () => {
+      console.log(` Worker ${process.pid} is listening on http://localhost:${PORT}`);
+    });
+  }
+};
+
+startServer();
